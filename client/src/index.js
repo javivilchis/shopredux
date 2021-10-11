de8b379d45546e952b151c10f0933f7ab92a3d9c
javier vilchis <javivilchis@gmail.com>
2021-10-10 20:43:10 -0500

    fixed the incompability for react, react-dom, react-redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import {Provider} from "react-redux";
+//redux - wire up the store
+import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
--- client/src/redux/Shopping/shopping-reducer.js
+++ client/src/redux/Shopping/shopping-reducer.js
@@ -7,40 +7,40 @@ const INITIAL_STATE = {
 }
 
 const shopReducer = (state = INITIAL_STATE, action) => {
-switch(action.type){
-     case actionTypes.ADD_TO_CART:
-          //get the items data from the products array
-          const item = state.products.find(prod => prod.id === action.payload.id)
-          //check if  item is in cart already
-          const inCart = state.cart.find(item => 
-               item.id === action.payload.id ? true : false
-               );
-          return {
-               ...state,
-               cart: inCart ? 
-               state.cart.map(item => item.id === action.payload.id ? { ...item, qty: item.qty + 1} 
-                    : item) : 
-               [...state.cart, {...item, qty: 1 }],
-          };
-     case actionTypes.REMOVE_FROM_CART:
-          return {
-               ...state,
-               cart: state.cart.filter(item => item.id !== action.payload.id),
-          };
-     case actionTypes.ADJUST_QTY:
-          return {
-               ...state,
-               cart: state.cart.map((item) => 
-                    item.id === action.payload.id ? {...item, qty: action.payload.qty} : item)
-          }; 
-     case actionTypes.LOAD_CURRENT_ITEM:
-          return {
-               ...state,
-               currentItem: action.payload,
-          };
-     default:
-          return state;
-}
+     switch(action.type){
+          case actionTypes.ADD_TO_CART:
+               //get the items data from the products array
+               const item = state.products.find(prod => prod.id === action.payload.id)
+               //check if  item is in cart already
+               const inCart = state.cart.find(item => 
+                    item.id === action.payload.id ? true : false
+                    );
+               return {
+                    ...state,//spread state
+                    cart: inCart ? 
+                    state.cart.map(item => item.id === action.payload.id ? { ...item, qty: item.qty + 1 } 
+                         : item) : 
+                    [...state.cart, {...item, qty: 1 }],
+               };
+          case actionTypes.REMOVE_FROM_CART:
+               return {
+                    ...state,
+                    cart: state.cart.filter(item => item.id !== action.payload.id),
+               };
+          case actionTypes.ADJUST_QTY:
+               return {
+                    ...state,
+                    cart: state.cart.map(item => 
+                         item.id === action.payload.id ? {...item, qty: action.payload.qty} : item)
+               }; 
+          case actionTypes.LOAD_CURRENT_ITEM:
+               return {
+                    ...state,
+                    currentItem: action.payload,
+               };
+          default:
+               return state;
+     }
 };
 
 export default shopReducer;
