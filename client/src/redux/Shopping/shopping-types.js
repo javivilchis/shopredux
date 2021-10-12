82ae0334ce34199dd62a9d6116da6f98b8da5d5c
javier vilchis <javivilchis@gmail.com>
2021-10-11 21:01:57 -0500

    update items for build

--- client/src/redux/Shopping/shopping-types.js
+++ client/src/redux/Shopping/shopping-types.js
@@ -2,3 +2,14 @@ export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const ADJUST_QTY = "ADJUST_QTY";
 export const LOAD_CURRENT_ITEM = "LOAD_CURRENT_ITEM";
+
+//original from utils/actions.js
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+
+export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
+export const CLEAR_CART = "CLEAR_CART";
+export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
+export const TOGGLE_CART = "TOGGLE_CART";
+
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
